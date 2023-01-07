8f880bcceaffe54aba960dd01fd34caddef25044
Window5 <13127531-Window5@users.noreply.gitlab.com>
2023-01-07 19:35:27 +0100

    Fix the permission error, maybe, again

--- src/main/java/me/window/permissions/SubPermission.java
+++ src/main/java/me/window/permissions/SubPermission.java
@@ -1,7 +1,5 @@
 package me.window.permissions;
 
-import java.util.ArrayList;
-
 /**
  *Subpermission of a {@link PermissionProvider PermissionProvider}
  */
@@ -31,5 +29,22 @@ public class SubPermission extends PermissionProvider {
     public SubPermission(PermissionProvider parent, int opLevel, String extensionPermission) {
         super(opLevel, parent.extensionPermission + "." + extensionPermission);
         this.parent = parent;
+    }
+
+    /**
+     * Replaces permission with new permissions
+     * @param extensionPermission The new permission
+     */
+    @Override
+    public void updatePermissions(String extensionPermission) {
+        super.updatePermissions(parent.extensionPermission + "." + extensionPermission);
+    }
+    /**
+     * Replaces permission with new permissions
+     * @param opLevel the new op level
+     * @param extensionPermission The new permission
+     */
+    public void updatePermissions(int opLevel, String extensionPermission) {
+        super.updatePermissions(opLevel, parent.extensionPermission + "." + extensionPermission);
     }
 }
